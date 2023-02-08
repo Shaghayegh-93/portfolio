639929d4beb8a412d960f98b8f48274067a35f53
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-09 01:35:15 +0330

    add a state for open and close of menu for mobile and change the code by add ternary oprator to it

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/logo.png";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log("open", isOpen);
+  // const toggleMenuHandler = () => {};
   return (
-    <div>
-      <div className="flex items-center justify-between px-4 fixed w-full h-[80px] bg-[#0a192f] text-gray-300">
-        <div>
-          <img src={logo} alt="logo" width="80px" />
-        </div>
-        <div>
-          <ul className="flex items-center justify-between">
-            <li>Home</li>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Work</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-        <div className="hidden">
-          <FaBars />
-        </div>
-        {/* mobile menu */}
-        <ul className="hidden">
+    <div className="flex items-center justify-between px-4 fixed w-full h-[80px] bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={logo} alt="logo" width="80px" />
+      </div>
+      <div>
+        <ul className="hidden md:flex items-center justify-between">
           <li>Home</li>
           <li>About</li>
           <li>Experience</li>
@@ -30,6 +20,24 @@ const Nav = () => {
           <li>Contact</li>
         </ul>
       </div>
+      <div className="md:hidden z-10 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+        {!isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      {/* mobile menu */}
+
+      <ul
+        className={
+          !isOpen
+            ? "  bg-[#0a192f] flex flex-col items-center justify-center h-screen w-full absolute top-0 left-0"
+            : "hidden"
+        }
+      >
+        <li className="py-6 text-3xl">Home</li>
+        <li className="py-6 text-3xl">About</li>
+        <li className="py-6 text-3xl">Experience</li>
+        <li className="py-6 text-3xl">Work</li>
+        <li className="py-6 text-3xl">Contact</li>
+      </ul>
     </div>
   );
 };
