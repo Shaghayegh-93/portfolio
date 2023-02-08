4b34889a63274842c55181dfd5a242fd693b63ae
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-08 23:53:53 +0330

    improve and expand code base on design of project

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import logo from "../assets/images/logo.png";
+import { FaBars } from "react-icons/fa";
 
 const Nav = () => {
   return (
     <div>
-      <div className="flex items-center justify-between px-4 fixed w-full h-[80px] bg-[#0a192f]">
+      <div className="flex items-center justify-between px-4 fixed w-full h-[80px] bg-[#0a192f] text-gray-300">
         <div>
           <img src={logo} alt="logo" width="80px" />
         </div>
         <div>
-          <ul>
+          <ul className="flex items-center justify-between">
             <li>Home</li>
             <li>About</li>
             <li>Experience</li>
@@ -17,6 +18,17 @@ const Nav = () => {
             <li>Contact</li>
           </ul>
         </div>
+        <div className="hidden">
+          <FaBars />
+        </div>
+        {/* mobile menu */}
+        <ul className="hidden">
+          <li>Home</li>
+          <li>About</li>
+          <li>Experience</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
       </div>
     </div>
   );
