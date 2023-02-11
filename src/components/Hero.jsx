c1f497f45340bb7246e30fdceb6ef30e44d25605
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-11 23:43:48 +0330

    expand style and improve code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,28 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Hero = () => {
   return (
-    <div className="grid grid-cols-5 ">
-      <div className="col-span-2"></div>
-      <div className="col-span-3">
-        <p>Hi my name is</p>
-        <p>Shaghayegh</p>
-        <p>I'm a FrontEnd Developer.</p>
-        <p>
+    <div name="hero" className=" bg-[#0a192f] h-screen w-full ">
+      <div className="max-w-[1000px] flex flex-col px-8 mx-auto h-full justify-center ">
+        <p className="text-[#E5E2E1]">Hi my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] ">
+          Shaghayegh
+        </h1>
+        <h2 className="text-[#8892b0] text-4xl sm:text-7xl font-bold">
+          I'm a FrontEnd Developer.
+        </h2>
+        <p className="text-[#8892b0] max-w-[65%] py-4">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum eaque
           ratione consequatur sit, eligendi odio voluptatem iure corrupti unde
-          voluptates nesciunt dolorem! Praesentium culpa consectetur dolorem
-          architecto fuga facere assumenda.
+          voluptates nesciunt dolorem!
         </p>
-        <button>
-          View Work <HiArrowNarrowRight />
-        </button>
+        <div>
+          <button className="border-2 text-white flex items-center justify-center px-6 py-3 my-2 hover:bg-[#E5E2E1] hover:text-black">
+            View Work
+            <span className="hover:rotate-90 duration-300 ">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
