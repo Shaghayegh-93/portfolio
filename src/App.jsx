0def6aa0e369f8d2bf83f65dffd2ec44309529d5
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-12 01:34:23 +0330

    import and render About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Hero/>
+      <Hero />
+      <About />
     </div>
   );
 }
