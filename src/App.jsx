84847e2ad322c11c7c64908754db32218627efff
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-12 20:59:53 +0330

    import and render Skills component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Hero />
       <About />
+      <Skills />
     </div>
   );
 }
