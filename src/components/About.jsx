fd4975d6c36a585992e0100cc1fd18f0c9a7a841
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-22 02:56:49 +0330

    improev project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,20 +14,31 @@ const About = () => {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
-            <p>Hi,I'm Shaghayegh,nice to meet you.Please take a look around</p>
+            <p>
+              Hi, I'm Shaghayegh, nice to meet you. Please take a look around
+            </p>
           </div>
           <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
-              numquam labore provident consequatur, perspiciatis omnis
-              dignissimos earum id accusamus nemo officia fuga. Dolor saepe
-              quae, odio non quidem beatae libero?
+            <p className="text-justify">
+              I'm an IT graduate always eager to learn and improve my skills in
+              front-end web development methods. In the recent few years, I've
+              done my best to learn different languages and frameworks like
+              JavaScript, React.js, Next.js, and Tailwind CSS.
+              <br />I have a good experience in working with Libraries, getting
+              data from APIs, and code based on websites’ design on
+              Figma.<br/> I have worked on many projects using React.js and Next.js, and I
+              know how to create effective, concise code while being able to
+              keep up with tight deadlines and be adaptive with my responses to
+              bugs and issues that arose.
+              <br /> Now, I see myself ready to climb up another step in my
+              career ladder. I do believe that I would be a passionate,
+              hardworking team player who always loves to challenge herself to
+              progress more and more.
             </p>
           </div>
         </div>
       </div>
     </div>
-
   );
 };
 
