1243abc32cd10eac8ebc9fe45ffdac22d9fd2f52
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-22 02:57:13 +0330

    improve project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,9 +14,9 @@ const Hero = () => {
           I'm a FrontEnd Developer.
         </h2>
         <p className="text-[#8892b0] max-w-[65%] py-4">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum eaque
-          ratione consequatur sit, eligendi odio voluptatem iure corrupti unde
-          voluptates nesciunt dolorem!
+          I'm an IT graduate with a few years of experience. My passion for
+          Front-End development always moves me forward to learn and improve my
+          skills in this field.
         </p>
         <div>
           <button className="border-2 text-white  px-6 py-3 my-2 hover:bg-[#E5E2E1] hover:text-black group">
