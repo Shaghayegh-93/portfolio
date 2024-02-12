b63e8ca26e91f6229b6415fc4633fc86c23f8a67
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-12 23:07:14 +0000

    add target ayytibute

--- src/components/work/WorkCard.jsx
+++ src/components/work/WorkCard.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 
-const WorkCard = ({ style, codeUrl, demoUrl }) => {
+const WorkCard = ({ style, codeUrl, demoUrl, title, createdBy }) => {
   return (
     <div
       style={style}
       className="shadow-lg shadow-[#040c16] flex flex-col items-center justify-center group container rounded-md mx-auto content-div"
     >
-      <div className="opacity-0 group-hover:opacity-100">
-        <span className="text-2xl font-bold text-white tracking-wider">
-          React Js Application
+      <div className="opacity-0 group-hover:opacity-100 mt-8  ">
+        <h2 className="text-2xl font-bold text-white text-center mb-2">
+          {title}
+        </h2>
+        <span className="text-lg font-bold text-white tracking-wider">
+          {createdBy}
         </span>
-        <div className="flex items-center justify-center mt-4">
+        <div className="flex items-center justify-center mt-2 w-full">
           <a
+            target="_blank"
             href={demoUrl}
-            className="text-lg text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold"
+            className="text-lg text-center rounded-lg w-1/2  px-2 py-2 m-2 bg-white text-gray-700 font-bold"
           >
             <button>Demo</button>
           </a>
           <a
+            target="_blank"
             href={codeUrl}
-            className="text-lg text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold"
+            className="text-lg text-center rounded-lg w-1/2 px-2  py-2 m-2 bg-white text-gray-700 font-bold"
           >
             <button>Code</button>
           </a>
